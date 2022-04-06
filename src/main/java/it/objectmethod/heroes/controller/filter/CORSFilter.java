813e10d474b97776323eb05de4403e8e3c0ee8a5
maiatteo <97740632+maiatteo@users.noreply.github.com>
2022-04-06 12:10:59 +0200

    Cors filter pushed perché se no non funziona

---
+++ src/main/java/it/objectmethod/heroes/controller/filter/CORSFilter.java
@@ -0,0 +1,27 @@
+package it.objectmethod.heroes.controller.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter {
+
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+		chain.doFilter(request, response);
+	}
+}
